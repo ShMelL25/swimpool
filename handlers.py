662621c35ab530facc73f25a10015ac93eb319c1
Egor Shchemelev <egor2014ag@mail.ru>
2024-04-20 08:03:26 +0300

    add_buttom

--- handlers.py
+++ handlers.py
@@ -2,39 +2,64 @@ from aiogram import types, F, Router, flags, Dispatcher
 from aiogram.types import Message
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
+from aiogram.types import (
+    CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, Message,
+)
+from aiogram.fsm.state import State, StatesGroup
 
 import text
-
+import kb
 from data_base.postgr_sql_base import SQL_request  
 
 router = Router()
 
+class SaveMessage(StatesGroup):
+    waiting_for_message = State()
 
-@router.message(F.text == "Привет!")
 @router.message(Command("start"))
 async def start_handler(msg: Message):
-    await msg.answer(text.greet.format(name=msg.from_user.full_name))
+    await msg.answer(text.greet.format(name=msg.from_user.full_name), reply_markup=kb.generate_menu(level='0').as_markup())
+    await msg.delete()
 
-@router.message(F.text == "Регистрация")
-@router.message(Command("registration"))
-async def register_handler(msg: Message):
+@router.callback_query(lambda callback: callback.data == "registr")
+async def register_handler(callback: CallbackQuery):
     
     ret = SQL_request().register_user(username=msg.from_user.full_name, telgram_id=msg.from_user.id)
-    await msg.reply(ret)  
+    await callback.message.answer('Test', reply_markup=kb.generate_menu(level='2.1').as_markup())
+    await callback.message.delete()
 
+@router.callback_query(lambda callback: callback.data == "train_edit")
+async def train_handler(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(SaveMessage.waiting_for_message)
+    await callback.message.answer(text.menu_train_edit, reply_markup=kb.generate_menu(level='1').as_markup())
+    await callback.message.delete()
 
-@router.message(Command("add"))
-async def add_handler(msg: Message):
-    print(msg.text)
-    ret = SQL_request().add_train(text=msg.text, telegram_id=msg.from_user.id)
-    await msg.reply(ret)  
+@router.callback_query(lambda callback: callback.data == "add_train")
+async def add_handler(callback: CallbackQuery, state: FSMContext):
+    
+    data = await state.get_data()
+    if 'saved_message' in data:
+        #ret = SQL_request().add_train(text=data['saved_message'], telegram_id=callback.message.from_user.id)
+        await callback.message.answer('Test', reply_markup=kb.generate_menu(level='2.1').as_markup())
+        
+    else:
+        await callback.message.answer("Вы ничего не добавляли!\nПопробуйте еще раз", reply_markup=kb.generate_menu(level='2.1').as_markup())
+    await state.clear()
+    await callback.message.delete()
+    
     
         
-@router.message(Command("del"))
-async def del_handler(msg: Message):
-    print(msg.text)
-    ret = SQL_request().del_train(text=msg.text, telegram_id=msg.from_user.id)
-    await msg.reply(ret)   
+@router.callback_query(lambda callback: callback.data == "del_train")
+async def del_handler(callback: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    if 'saved_message' in data:
+        #ret = SQL_request().del_train(text=data['saved_message'], telegram_id=callback.message.from_user.id)
+        await callback.message.answer('Test', reply_markup=kb.generate_menu(level='2.1').as_markup())
+        
+    else:
+        await callback.message.answer("Вы ничего не добавляли!\nПопробуйте еще раз", reply_markup=kb.generate_menu(level='2.1').as_markup())
+    await state.clear()
+    await callback.message.delete()   
     
 @router.message(Command("doc"))
 async def doc_handler(msg: Message):
@@ -49,3 +74,16 @@ async def dev_request(msg: Message):
     t = t.split(' ;')[1].split(';')
     ret = SQL_request().request_dev(req=t[0], password=t[1])
     await msg.reply(ret)  
+    
+    
+@router.message(SaveMessage.waiting_for_message)
+async def save_message(message: Message, state: FSMContext):
+    data = await state.get_data()
+    data['saved_message'] = message.text
+    await state.update_data(data)
+    await message.delete()
+    
+@router.callback_query(lambda callback: callback.data == "menu")
+async def menu(callback: CallbackQuery):
+    await callback.message.answer(text.greet, reply_markup=kb.generate_menu(level='0').as_markup())
+    await callback.message.delete()
